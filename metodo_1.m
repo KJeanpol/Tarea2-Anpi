c4dbbd7c6b3efcd44e6c5ec42bdec1ae5cc0ebff
Jonathan G Araya <jonathana1196@gmail.com>
2020-06-28 23:20:48 -0600

    Falta ultima parte

---
+++ metodo_1.m
@@ -0,0 +1,50 @@
+warning('off')
+pkg load symbolic
+function [x_k,itera] = metodo_1(F,vars,x_0,tol) 
+    w=[];
+    x=[];
+    y=[];
+    z=[];
+    error=[];
+    a=1;
+    b=-2;
+    itera=0;
+    iteraciones=[];
+    ep=tol+1;
+    JF= jacobian(F,vars);
+
+    while(ep>tol)
+        Jx_kF = linsolve(double(Fx(JF,vars,x_0)), double(Fx(F, vars, x_0))');
+
+        y_k=x_0 - (1/2)*(Jx_kF');
+
+        JF_sol = linsolve(double(Fx(JF,vars,y_k)), double(-Fx(F, vars, x_0))');
+
+        z_k = x_0 + JF_sol';
+
+        M = double(a * Fx(JF,vars,x_0) + b *  Fx(JF,vars,y_k));
+
+        Fz= double(Fx(F,vars,z_k));
+
+        M_solve = linsolve(M,-Fz');
+
+        x_k = z_k + M_solve';
+
+        x_0=x_k;
+
+        itera = itera+ 1;
+        iteraciones=[iteraciones itera];
+        w=[w x_0(1)];
+        x=[x x_0(2)];
+        y=[y x_0(3)];
+        z=[z x_0(4)];
+        error=[error ep];
+        ep=double(norm(Fx(F,vars,x_0)));
+    endwhile
+    plot(iteraciones,error,'b--o') 
+     
+endfunction
+
+function F = Fx(f,vars,x_0) 
+    F=subs(f, vars, x_0);     
+endfunction
---
+++ newton_raphson_nl.m
@@ -0,0 +1,30 @@
+warning('off')
+function [xk,k,err] = newton_raphson_nl(f,vars,x0,tol,maxIter)
+  syms x y z
+  xk=x0';
+  Jf=jacobian(f,vars);
+  k=0;
+  err=tol+1;
+  iteraciones=[0];
+  error=[err];
+  while(k<maxIter&& err>tol)
+    Jf_xk=subs(Jf, vars, xk);
+    f_xk=(subs(f, vars, xk))';
+    xk= xk-linsolve(double(Jf_xk), double(f_xk));
+    k=k+1;
+    f_xk=double(subs(f, vars, xk));
+    err=double(norm(f_xk));
+    iteraciones=[iteraciones k];
+    error=[error err];
+  endwhile
+  plot(iteraciones,error,'b--o')
+endfunction
+
+pkg load symbolic
+syms x y z
+f=[cos(y)-cos(x),(z**x)-(1/y),exp(x)-(z**2)];
+vars=[x,y,z];
+x0=[0.5 0.5 0.5];
+tol=0.00001;
+maxIter=1000;
+[xk,k,err]=newton_raphson_nl(f,vars,x0,tol,maxIter)
